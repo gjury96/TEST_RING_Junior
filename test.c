5fa35ef739bde090907bbfb52e30a2346989d615
Jury Halma <juryhalma@MacBook-Air-Jury.local>
2020-10-19 01:44:49 +0300

    CRC32 bugs

--- test.c
+++ test.c
@@ -14,21 +14,24 @@ int main (int argc, char **argv)
   gen_crc_table();
   if (argc == 1) 
   {
-          printf("-h help\n");
-          return 0;
-      }
+    printf("-h help\n");
+    return 0;
+  }
 
-     if (argc < 5 || argc > 6) {
-    if (strcmp(argv[1], "-h") == 0) {
+  if (argc < 5 || argc > 6) 
+  {
+    if (strcmp(argv[1], "-h") == 0) 
+    {
       printf("Parameter order: key, input file location, output file location, encryption key location\n");
       printf("-d decrypts");
       printf("-e encrypts");
-      }
-     else {
-          printf("Invalid number of parameters\n");
-          return -1;
-     }
     }
+    else 
+    {
+        printf("Invalid number of parameters\n");
+        return -1;
+    }
+  }
 
 //--------------------encodind
   else if (strcmp(argv[1], "-e") == 0)
@@ -39,18 +42,18 @@ int main (int argc, char **argv)
 
           if (f1 == NULL) 
       {
-          printf("Invalid file address %s\n", argv[2]);
-          return -1;
+        printf("Invalid file address %s\n", argv[2]);
+        return -1;
       }
       else if (f2 == NULL)
       {
-          printf("Invalid file address %s\n", argv[3]);
-          return -1;
+        printf("Invalid file address %s\n", argv[3]);
+        return -1;
       }
-          else if (f3 == NULL)
+        else if (f3 == NULL)
       {
-          printf("Invalid file address %s\n", argv[4]);
-          return -1;
+        printf("Invalid file address %s\n", argv[4]);
+        return -1;
       }
      int Leg = 16;
 
@@ -74,19 +77,11 @@ struct AES_ctx ctx;
 
     // // fputs (Word, f2);  //writing header to file
     // // fprintf(f2, "%d\t", size);
-    // // fprintf(f2, "%d\n", crc32);
+    fprintf(f2, "%d\n", crc32);
 
       for (int i = 0; i < 16; i++)
        fprintf(f2, "0x%x\n", datae[i]);
     
-    // // for (int i = 0; i < 256; i++) //bit format of output data
-    // // {
-    // //   for (int j = 0; j < 8; j++) 
-    // //   {
-    // //     fprintf(f2, "0x%x\t", (out[i] & 0x80) ? '1' : '0'))
-    // //     out[i] <<= 1;
-    // //   }
-    // // }
     printf("%s %d %d\n", Word, size, crc32);
 
 
@@ -102,19 +97,22 @@ struct AES_ctx ctx;
       f3 = fopen(argv[4], "r");
           if (f1 == NULL) 
       {
-                printf("Invalid file address %s\n", argv[2]);
-                return -1;
+        printf("Invalid file address %s\n", argv[2]);
+        return -1;
       }
       else if (f2 == NULL)
       {
-          printf("Invalid file address %s\n", argv[3]);
-          return -1;
+        printf("Invalid file address %s\n", argv[3]);
+        return -1;
       }
-            else if (f3 == NULL)
+        else if (f3 == NULL)
       {
-          printf("Invalid file address %s\n", argv[4]);
-          return -1;
+        printf("Invalid file address %s\n", argv[4]);
+        return -1;
       }
+    uint16_t CRC32_after;
+    fscanf(f1, "%d", &CRC32_after);
+
     for (int i = 0; i < 16; i++)
     fscanf(f1, "%x", &datad[i]);
     fclose(f1);
@@ -124,17 +122,17 @@ struct AES_ctx ctx;
     fclose(f3);
   
     int Leg = 16;
-    uint16_t b = update_crc(-1, datad, Leg);
-    printf("%d\n", b);
+    uint16_t CRC32_before = update_crc(-1, datad, Leg);
 
-struct AES_ctx ctx;
+    if (CRC32_after == CRC32_before) printf("SUCCESS CRC32!!!\n");
+    else printf("EROR CRC32\n");
+    printf("%d\n", CRC32_after);
+    printf("%d\n", CRC32_before);
+
+    struct AES_ctx ctx;
 
     AES_init_ctx(&ctx, key);
     AES_ECB_decrypt(&ctx, datad);
-
-// size = ftell(f2);
-    // for (int i = 0; i < 16; i++)
-    //   out[i] = out[i+1];
 
 for (int i = 0; i < 16; i++)
        fprintf(f2, "0x%x\n", datad[i]);  
